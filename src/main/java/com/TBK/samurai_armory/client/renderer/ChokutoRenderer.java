db4552a63789e087aa8684080147de9d39481228
AstianBK <Agustinmarin2016@gmail.com>
2025-01-06 21:32:49 -0300

    First Commit.

--- src/main/java/com/TBK/samurai_armory/client/renderer/ChokutoRenderer.java
+++ src/main/java/com/TBK/samurai_armory/client/renderer/ChokutoRenderer.java
@@ -2,19 +2,10 @@ package com.TBK.samurai_armory.client.renderer;
 
 import com.TBK.samurai_armory.client.model.ChokutoModel;
 import com.TBK.samurai_armory.common.items.ChokutoItem;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.resources.ResourceLocation;
-import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.renderer.GeoItemRenderer;
 
 public class ChokutoRenderer<T extends ChokutoItem> extends GeoItemRenderer<T> {
     public ChokutoRenderer() {
         super(new ChokutoModel<>());
-    }
-
-    @Override
-    public RenderType getRenderType(T animatable, ResourceLocation texture, @Nullable MultiBufferSource bufferSource, float partialTick) {
-        return RenderType.itemEntityTranslucentCull(texture);
     }
 }
--- src/main/java/com/TBK/samurai_armory/common/items/SpearItem.java
+++ src/main/java/com/TBK/samurai_armory/common/items/SpearItem.java
@@ -18,14 +18,17 @@ import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache
 import software.bernie.geckolib.core.animation.AnimatableManager;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
+import java.util.UUID;
 import java.util.function.Consumer;
 
 public class SpearItem extends SwordItem implements GeoItem {
+    private static final UUID TOOL_MODIFIER_UUID = UUID.fromString("123e4567-e89b-12d3-a456-426614174000"); // Genera un UUID único
+
     private final float attackSpeed;
     private final AnimatableInstanceCache cache= GeckoLibUtil.createInstanceCache(this);
     public SpearItem(Properties p_43272_) {
-        super(Tiers.IRON, 3, 1.0F, p_43272_);
-        this.attackSpeed =1.0F;
+        super(Tiers.WOOD, 0, 0.0F, p_43272_);
+        this.attackSpeed =-2.8F;
     }
     @Override
     public void initializeClient(Consumer<IClientItemExtensions> consumer) {
@@ -58,8 +61,8 @@ public class SpearItem extends SwordItem implements GeoItem {
 
     public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlot p_43383_) {
         ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
-        builder.put(ForgeMod.ENTITY_REACH.get(), new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Tool modifier", this.getAttackRange(), AttributeModifier.Operation.ADDITION));
-        builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Tool modifier", this.getAttackDamage(), AttributeModifier.Operation.ADDITION));
+        builder.put(ForgeMod.ENTITY_REACH.get(), new AttributeModifier(TOOL_MODIFIER_UUID,"Tool modifier", this.getAttackRange(), AttributeModifier.Operation.ADDITION));
+        builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Tool modifier", this.getAttackDamage()-1, AttributeModifier.Operation.ADDITION));
         builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Tool modifier", (double)this.attackSpeed, AttributeModifier.Operation.ADDITION));
         return p_43383_ == EquipmentSlot.MAINHAND ? builder.build() : super.getDefaultAttributeModifiers(p_43383_);
     }
